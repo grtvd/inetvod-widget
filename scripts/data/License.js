1d3878988030679f62d324cde61a2f88b69d620d
grtvd <grtvd@users.noreply.github.com>
2006-10-05 22:59:58 -0400

    widget: Added ContentID, UserID, and Password to data/License.

--- scripts/data/License.js
+++ scripts/data/License.js
@@ -5,6 +5,9 @@
 
 License.ShowURLMaxLength = 4096;
 License.LicenseURLMaxLength = 4096;
+License.ContentIDMaxLength = 64;
+License.UserIDMaxLength = 64;
+License.PasswordMaxLength = 32;
 
 /******************************************************************************/
 
@@ -13,6 +16,9 @@ function License(reader)
 	this.LicenseMethod = null;
 	this.ShowURL = null;
 	this.LicenseURL = null;
+	this.ContentID = null;
+	this.UserID = null;
+	this.Password = null;
 
 	if(reader != undefined)
 		this.readFrom(reader);
@@ -25,6 +31,9 @@ function License(reader)
 	this.LicenseMethod = reader.readString("LicenseMethod", LicenseMethodMaxLength);
 	this.ShowURL = reader.readString("ShowURL", License.ShowURLMaxLength);
 	this.LicenseURL = reader.readString("LicenseURL", License.LicenseURLMaxLength);
+	this.ContentID = reader.readString("ContentID", License.ContentIDMaxLength);
+	this.UserID = reader.readString("UserID", License.UserIDMaxLength);
+	this.Password = reader.readString("Password", License.MaxLength);
 }
 
 /******************************************************************************/
